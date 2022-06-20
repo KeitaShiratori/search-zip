ef87526eb1de301ebcf2bea850c3fa140c39fc47
KeitaShiratori <keita.shiratori@gmail.com>
2022-06-20 23:59:25 +0900

    feat: add GET /searchZip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,37 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!"+JSON.stringify(request.query), {structuredData: true});
+const URL = "https://google.co.jp/maps/search/";
 
-  
-  response.send("Hello from Firebase!");
-});
+export const searchZip = functions.https.onRequest(
+  async (request, response) => {
+    functions.logger.info("Hello logs!" + JSON.stringify(request.query), {
+      structuredData: true,
+    });
+
+    if (!request.query?.address?.length) {
+      response.status(400).send({
+        message: "Invalid Request. usage: GET /searchZip?address=value",
+      });
+    }
+
+    const address = request.query.address;
+    const { data } = await axios({
+      method: "GET",
+      url: encodeURI(URL + address),
+    });
+
+    console.log(JSON.stringify(data, null, "  "));
+    const m = data.match(/〒(\d{3}-\d{2,4})/);
+    if (!m) {
+      response.status(404).send({
+        message: "Not Found",
+      });
+    } else {
+      response.send({ zipCode: m[1] });
+    }
+  }
+);
